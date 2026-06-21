78738c76d2f45d78edbcbf70156de4a9be0b8a16
se8ge <153080841+se8ge@users.noreply.github.com>
2026-06-22 00:40:28 +0200

    feat(terraform): update S3 backend bucket and add variable defaults for new AWS account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "stocklive-terraform-state"
+    bucket         = "stocklive-tfstate-443628962197"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "terraform-lock"
